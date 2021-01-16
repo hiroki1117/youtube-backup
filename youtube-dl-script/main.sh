7237cbf136a61ff40742f02fcb1e3a6da4ddb3fc
hiroki1117 <scala1117@gmail.com>
2021-01-17 01:16:55 +0900

    webm対応

--- youtube-dl-script/main.sh
+++ youtube-dl-script/main.sh
@@ -7,4 +7,9 @@ echo $filename
 
 youtube-dl -o "$filename" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 "$URL"
 
+#webmとかでファイル名が異なる場合はmp4に修正
+if [ ! -f $filename ]; then
+  filename=${filename}.mp4
+fi
+
 aws s3 cp "${filename}" s3://youtubedl-bucket/test/
