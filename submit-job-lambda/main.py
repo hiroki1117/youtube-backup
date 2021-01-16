22d13319cfff797eb90963eb5ff1b3f8429492eb
hiroki1117 <scala1117@gmail.com>
2021-01-17 00:41:47 +0900

    Youtubedlできる

--- submit-job-lambda/main.py
+++ submit-job-lambda/main.py
@@ -2,17 +2,31 @@ import boto3
 import json
 
 def lambda_handler(event, context):
+    url = event['queryStringParameters']['url']
     
     client = BatchClient()
     queue = "youtubedl-batch-queue4"
-    jobDefinition = "sample-youtube-dl:6"
+    jobDefinition = "sample-youtube-dl:7"
     jobname = "youtubedljob-from-lambda"
+    containerOverrides={
+        'environment': [
+            {
+                'name': 'URL',
+                'value': url
+            },
+        ]
+    }
     
-    client.submit_job(jobname, queue, jobDefinition, {})
-    
+    client.submit_job(
+        job_name=jobname,
+        job_queue=queue,
+        job_definition=jobDefinition,
+        container_overrides=containerOverrides
+        )
+     
     response = {
         "statusCode": 200,
-        "body": "Hello from Lambda!!!!",
+        "body": url,
         "headers": {
             "Content-Type": "application/json"
         }
@@ -30,6 +44,6 @@ class BatchClient:
     self.client.submit_job(
       jobName=job_name,
       jobQueue=job_queue,
-      jobDefinition=job_definition
-    #   containerOverrides=container_overrides
+      jobDefinition=job_definition,
+      containerOverrides=container_overrides
     )
--- youtube-dl-script/main.sh
+++ youtube-dl-script/main.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-youtube-dl -o "hoge" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 "https://www.youtube.com/watch?v=F0_tR6kZVI4"
+filename=$(youtube-dl --get-filename $URL)
 
-aws s3 cp hoge.mp4 s3://youtubedl-bucket/test/
+echo $URL
+echo $filename
+
+youtube-dl -o "$filename" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 "$URL"
+
+aws s3 cp "${filename}" s3://youtubedl-bucket/test/
