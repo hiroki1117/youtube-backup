c9839216f962ba9139955453f2a10366421ce421
hiroki <scala1117@gmail.com>
2022-01-27 13:16:28 +0900

    dockerイメージをアップデート

--- youtube-dl-script/main.sh
+++ youtube-dl-script/main.sh
@@ -5,6 +5,8 @@ echo $URL
 echo $FILENAME
 
 # youtube-dl -o "$FILENAME" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 "$URL"
-youtube-dl -o "$FILENAME" -f 'best[ext=mp4]' "$URL"
+# youtube-dl -o "$FILENAME" -f "best[ext=mp4]" "$URL"
+youtube-dl -o "$FILENAME" -f "best[ext=mp4]/best" --merge-output-format mp4 "$URL"
 
 aws s3 cp "${FILENAME}" "${S3PATH}"
+
