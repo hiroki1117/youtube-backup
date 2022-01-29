2584c84028bb3fb2e84df23be83e37cdcb7ce343
hiroki <scala1117@gmail.com>
2022-01-30 02:59:42 +0900

    コメント追記

--- youtube-dl-script/main.sh
+++ youtube-dl-script/main.sh
@@ -8,5 +8,6 @@ echo $FILENAME
 # youtube-dl -o "$FILENAME" -f "best[ext=mp4]" "$URL"
 youtube-dl -o "$FILENAME" -f "best[ext=mp4]/best" --merge-output-format mp4 "$URL"
 
+# 動画をS3にアップロード
 aws s3 cp "${FILENAME}" "${S3PATH}"
 
