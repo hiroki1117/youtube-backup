ff9834d17fc23a5e47882856ff6a638f5db12e50
hiroki <scala1117@gmail.com>
2022-01-30 14:53:30 +0900

    修正

--- complete-video-upload/main.py
+++ complete-video-upload/main.py
@@ -5,11 +5,14 @@ import json
 def lambda_handler(event, context):
     object_key = event['Records'][0]['s3']['object']['key']
     video_id = os.path.basename(object_key).split('.')[0]
-    update_video_upload_status(video_id)
+    update_video_upload_status(video_id_special_process(video_id))
+    print(video_id)
+    print(video_id_special_process(video_id))
 
-def update_video_upload_status(video_id_special_process(video_id)):
+def update_video_upload_status(video_id):
     dynamo_client = boto3.resource("dynamodb")
     table = dynamo_client.Table(os.environ["DYNAMO_TABLE_NAME"])
+
     # ここでS3のパスも更新するようにしたらmp4以外にも対応できるかも
     response = table.update_item(
         Key={
@@ -21,10 +24,11 @@ def update_video_upload_status(video_id_special_process(video_id)):
         },
         ReturnValues="UPDATED_NEW"
     )
+    print(response)
     return response
 
 # IDが-から始まる場合は特別な処理をしているのでその変換
 def video_id_special_process(video_id):
     if video_id.startswith("ABCXYZ-"):
-        return video_id[7:]
+        return video_id[6:]
     return video_id
