e149123561e7f30e0ddd43019ca4a6aaa87d972e
hiroki <scala1117@gmail.com>
2022-02-02 09:50:09 +0900

    動画dl時間が短くなるか実験

--- youtube-dl-script/main.sh
+++ youtube-dl-script/main.sh
@@ -6,8 +6,8 @@ echo $FILENAME
 echo ${S3PATH}
 
 # youtube-dl -o "$FILENAME" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 "$URL"
-# youtube-dl -o "$FILENAME" -f "best[ext=mp4]" "$URL"
-youtube-dl -o "$FILENAME" -f "best[ext=mp4]/best" --merge-output-format mp4 "$URL"
+youtube-dl -o "$FILENAME" -f "best[ext=mp4]" "$URL"
+# youtube-dl -o "$FILENAME" -f "best[ext=mp4]/best" --merge-output-format mp4 "$URL"
 
 # 動画をS3にアップロード
 aws s3 cp "${FILENAME}" "${S3PATH}"
