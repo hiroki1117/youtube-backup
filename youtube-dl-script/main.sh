48f4be00828a0614f7f0a83f3a976673c612c33e
hiroki <scala1117@gmail.com>
2022-02-02 11:36:03 +0900

    youtube-dlのコマンド修正

--- youtube-dl-script/main.sh
+++ youtube-dl-script/main.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-set -eu
+set -u
 
 echo $URL
 echo $FILENAME
 echo ${S3PATH}
 
 # youtube-dl -o "$FILENAME" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best" --merge-output-format mp4 "$URL"
-youtube-dl -o "$FILENAME" -f "best[ext=mp4]" "$URL"
+# youtube-dl -o "$FILENAME" -f "best[ext=mp4]" "$URL"
 # youtube-dl -o "$FILENAME" -f "best[ext=mp4]/best" --merge-output-format mp4 "$URL"
+# mp4でダウンロードが失敗したらマージでダウンロードする
+youtube-dl -o "$FILENAME" -f "best[ext=mp4]" "$URL" || youtube-dl -o "$FILENAME" -f "best[ext=mp4]/best" --merge-output-format mp4 "$URL"
+
 
 # 動画をS3にアップロード
 aws s3 cp "${FILENAME}" "${S3PATH}"
