5eba98bb477abff8e3fcebf0fb9578d546badd3e
hiroki1117 <scala1117@gmail.com>
2022-02-08 00:01:12 +0900

    youtube upload修正

--- youtube-upload-script/main.sh
+++ youtube-upload-script/main.sh
@@ -5,6 +5,10 @@ echo $S3PATH
 echo $VIDEO_TITLE
 echo $PRIVACY_STATUS
 
+# S3からアクセストークンを取得(一時処置)
+echo $S3TOKEN
+aws s3 cp "${S3PATH}" "token.json"
+
 # S3のパスから動画ファイル名を取得
 array=( `echo $S3PATH | tr -s '/' ' '`)
 last_index=`expr ${#array[@]} - 1`
--- youtube-upload-script/upload_video.py
+++ youtube-upload-script/upload_video.py
@@ -2,15 +2,15 @@ import http.client
 import httplib2
 import os
 import random
-import sys
 import time
+import argparse
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
 
-from apiclient.discovery import build
-from apiclient.errors import HttpError
-from apiclient.http import MediaFileUpload
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
 
 
 httplib2.RETRIES = 1
@@ -26,6 +26,18 @@ RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error,
                         http.client.BadStatusLine)
 RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 CLIENT_SECRETS_FILE = "client_secrets.json"
+OAUTH_TOKEN_FILE = "token.json"
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+   %s
+with information from the API Console
+https://console.developers.google.com/
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
 
 YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
 YOUTUBE_API_SERVICE_NAME = "youtube"
@@ -36,16 +48,13 @@ VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
 
 def get_authenticated_service(args):
-    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-                                   scope=YOUTUBE_UPLOAD_SCOPE,
-                                   message="MISSING_CLIENT_SECRETS_ERROR")
+    creds = None
+    if os.path.exists(OAUTH_TOKEN_FILE):
+        creds = Credentials.from_authorized_user_file(OAUTH_TOKEN_FILE, YOUTUBE_UPLOAD_SCOPE)
+    if not creds or not creds.valid:
+        raise Exception("tokenが不正")
 
-    storage = Storage("oauth2.json")
-    credentials = storage.get()
-
-    return build(YOUTUBE_API_SERVICE_NAME,
-                 YOUTUBE_API_VERSION,
-                 http=credentials.authorize(httplib2.Http()))
+    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=creds)
 
 
 def initialize_upload(youtube, options):
@@ -107,6 +116,7 @@ def resumable_upload(insert_request):
 
 
 if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
     argparser.add_argument("--file", required=True, help="Video file to upload")
     argparser.add_argument("--title", help="Video title", default="Test Title")
     argparser.add_argument("--description",
@@ -128,6 +138,6 @@ if __name__ == '__main__':
     youtube = get_authenticated_service(args)
     try:
         initialize_upload(youtube, args)
-    except HttpError as e:
+    except Exception as e:
         print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
-
+        
