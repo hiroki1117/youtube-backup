f3446348f8a8d5e82df2aa8bc9b21b65c0debba7
hiroki1117 <scala1117@gmail.com>
2024-10-06 17:36:03 +0900

    ytdlpの動作を完全に停止

--- script/yt-dlp-script/main.sh
+++ script/yt-dlp-script/main.sh
@@ -8,9 +8,12 @@ echo ${S3PATH}
 
 # mp4でダウンロードが失敗したらマージでダウンロードする
 #yt-dlp --recode-video mp4 -o "$FILENAME" "$URL"
-yt-dlp -o "$FILENAME.%(ext)s" "$URL"
 
-FULLFILENAME=`find $FILENAME.*`
+# https://github.com/yt-dlp/yt-dlp/issues/10128 の問題のためしばらくの間は完全に動作を止めておく
 
+# yt-dlp -o "$FILENAME.%(ext)s" "$URL"
+#
+# FULLFILENAME=`find $FILENAME.*`
+#
 # 動画をS3にアップロード
-aws s3 cp "${FULLFILENAME}" "${S3PATH}"
+# aws s3 cp "${FULLFILENAME}" "${S3PATH}"
